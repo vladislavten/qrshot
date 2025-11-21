05baea581d93b54fcca88e7000c8ae7187aaf0d7
vladislavten <vladislav.ten1987@gmail.com>
2025-11-21 13:13:10 +0500

    settings for web

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,8 @@
 const API_CONFIG = {
-    baseUrl: 'http://localhost:5000/api', // заменить когда буду переходить на боевой режим
+    //baseUrl: 'http://localhost:5000/api', // заменить когда буду переходить на боевой режим
     //baseUrl: 'http://192.168.100.30:5000/api', // заменить когда буду переходить на боевой режим
+    baseUrl: 'http://qrshot.kz:5000/api', // заменить когда буду переходить на боевой режим
+
     endpoints: {
         events: '/events',
         photos: '/photos',
